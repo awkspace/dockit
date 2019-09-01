27bc7d737a7a4adf34dd5d853e8c902b109fa94c
awk <self@awk.space>
2019-09-01 17:09:58 -0400

    Add installer

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ ! $(id -u) -eq 0 ]
+then
+    echo "The installer requires root privileges."
+    echo "If you'd like to run dockit as a user, add the bin/ directory to your PATH."
+    exit 1
+fi
+
+dir=$(dirname "$0")
+
+cp $dir/bin/dockit /usr/local/bin/dockit
+cp -r $dir/lib/dockit/ /usr/local/lib/dockit/
