016921fb13b1f0772addf207558d3981f6f69d63
Tyler <4926565+tyler71@users.noreply.github.com>
2020-09-27 22:36:12 -0700

    Add completion for bash & zsh
    For a completion script to be installed, a few conditions need to be met
    * The shell should be installed
    * docker installed and the user part of the docker group
    
    Additional message for zsh users to include bashcompinit
    
    zsh version has autocompletion, bash will simply list all existing images.

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
-#!/bin/sh
+#!/bin/bash
+set -e
+
+bash_complete_dir="/etc/bash_completion.d"
+zsh_complete_dir="/usr/share/zsh/vendor-completions"
 
 if [ ! $(id -u) -eq 0 ]
 then
@@ -9,5 +13,32 @@ fi
 
 dir=$(dirname "$0")
 
-cp $dir/bin/dockit /usr/local/bin/dockit
-cp -r $dir/lib/dockit /usr/local/lib
+cp --update --verbose             $dir/bin/dockit /usr/local/bin/dockit
+cp --update --verbose --recursive $dir/lib/dockit /usr/local/lib
+
+#   Ensure that docker is installed and you are part of the docker group.
+#   This command will add you to the docker group:
+#   $usermod -a -G docker myusername"
+if command -v bash   &> /dev/null \
+    && command -v docker &> /dev/null \
+    && id -nG "$(whoami)" | grep -qw "docker"
+then
+    mkdir -p "$bash_complete_dir"
+    cp --update --verbose misc/dockit.bashcompletion "$bash_complete_dir"/dockit
+    echo "bash completion script installed"
+else
+    echo "bash completion script not installed"
+fi
+
+if command -v zsh    &> /dev/null \
+    && command -v docker &> /dev/null \
+    && id -nG "$(whoami)" | grep -qw "docker"
+then
+    mkdir -p "$zsh_complete_dir"
+    cp --update --verbose misc/dockit.zshcompletion "$zsh_complete_dir"/_dockit
+    echo "zsh completion script installed"
+    echo -e "\nYou may need to include this line in your ~/.zshrc file\n"
+    echo -e "    autoload bashcompinit && bashcompinit\n"
+else
+    echo "zsh completion script not installed"
+fi
